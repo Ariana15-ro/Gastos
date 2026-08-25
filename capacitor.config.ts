61e126b7e586b98b8455041f5559f26756f134c3
Ariana <arianarodriguez1315@gmail.com>
2026-08-25 17:46:52 -0500

    Add Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gastos.app',
+  appName: 'Gastos',
+  webDir: 'dist'
+};
+
+export default config;
